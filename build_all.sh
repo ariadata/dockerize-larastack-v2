1c0824452f045b8bed11967c067eda09aabe75fa
Mehrdad Amini <mehrdad@arzinja.dev>
2023-06-04 01:36:21 +0300

    fix permission of root dir in tinker and other webuser

---
+++ build_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Building 7.4" && cd ./7.4/ && bash build.sh && cd ..
+echo "Building 8.0" && cd ./8.0/ && bash build.sh && cd ..
+echo "Building 8.1" && cd ./8.1/ && bash build.sh && cd ..
+echo "Building 8.2" && cd ./8.2/ && bash build.sh && cd ..
---
+++ push_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Start pushing 7.4" && cd ./7.4/ && bash push.sh && cd ..
+echo "Start pushing 8.0" && cd ./8.0/ && bash push.sh && cd ..
+echo "Start pushing 8.1" && cd ./8.1/ && bash push.sh && cd ..
+echo "Start pushing 8.2" && cd ./8.2/ && bash push.sh && cd ..
