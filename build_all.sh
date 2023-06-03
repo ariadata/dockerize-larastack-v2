92653b01316ef39b45aecd985e4ac4692ffa8201
Mehrdad Amini <mehrdad@arzinja.dev>
2023-06-04 01:47:10 +0300

    add remove old images command

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# remove all images with pattern ariadata/php-:*
+docker rmi -f $(docker images | grep ariadata/php- | awk '{print $3}')
+
 echo "Building 7.4" && cd ./7.4/ && bash build.sh && cd ..
 echo "Building 8.0" && cd ./8.0/ && bash build.sh && cd ..
 echo "Building 8.1" && cd ./8.1/ && bash build.sh && cd ..
