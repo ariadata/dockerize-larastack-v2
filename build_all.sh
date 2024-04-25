c6585e5c0d1914c407a0e0e2cd8522c7a74d7f93
Mehrdad Amini <mehrdad@arzinja.dev>
2024-04-25 15:01:14 +0300

    archived some php versions

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@
 docker rmi -f $(docker images | grep ariadata/php- | awk '{print $3}')
 
 echo "Building 7.4" && cd ./7.4/ && bash build.sh && cd ..
-echo "Building 8.0" && cd ./8.0/ && bash build.sh && cd ..
-echo "Building 8.1" && cd ./8.1/ && bash build.sh && cd ..
+# echo "Building 8.0" && cd ./8.0/ && bash build.sh && cd ..
+# echo "Building 8.1" && cd ./8.1/ && bash build.sh && cd ..
 echo "Building 8.2" && cd ./8.2/ && bash build.sh && cd ..
 echo "Building 8.3" && cd ./8.3/ && bash build.sh && cd ..
