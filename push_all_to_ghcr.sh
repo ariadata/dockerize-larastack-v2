3eab4930c310a3ff209dc174c2d939a1b3201498
Mehrdad Amini <mehrdad@arzinja.dev>
2025-06-03 11:10:17 +0300

    Revert "archived some php versions"
    This reverts commit c6585e5c0d1914c407a0e0e2cd8522c7a74d7f93.

--- push_all_to_ghcr.sh
+++ push_all_to_ghcr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start pushing 7.4" && cd ./7.4/ && bash push_to_ghcr.sh && cd ..
-# echo "Start pushing 8.0" && cd ./8.0/ && bash push_to_ghcr.sh && cd ..
-# echo "Start pushing 8.1" && cd ./8.1/ && bash push_to_ghcr.sh && cd ..
+echo "Start pushing 8.0" && cd ./8.0/ && bash push_to_ghcr.sh && cd ..
+echo "Start pushing 8.1" && cd ./8.1/ && bash push_to_ghcr.sh && cd ..
 echo "Start pushing 8.2" && cd ./8.2/ && bash push_to_ghcr.sh && cd ..
 echo "Start pushing 8.3" && cd ./8.3/ && bash push_to_ghcr.sh && cd ..
