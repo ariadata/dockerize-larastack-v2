77f6b78518bf2519c0c41874469a4789fc1d3485
Mehrdad Amini <mehrdad@arzinja.dev>
2025-06-03 11:02:40 +0300

    upgrade 8.4

---
+++ 8.4/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+PHP_VERSION=8.4
+
+docker build -t ariadata/php-$PHP_VERSION:cli ./cli/
+
+docker build -t ariadata/php-$PHP_VERSION:fpm ./fpm/
+
+docker build -t ariadata/php-$PHP_VERSION:fpm-nginx ./fpm-nginx/
+
+docker build -t ariadata/php-$PHP_VERSION:supervisor ./supervisor/
+
+echo "\n🔥 Build Done!"
